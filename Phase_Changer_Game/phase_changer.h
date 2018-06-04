e3f339a3ba6f1e30cd108b668da108a46c720138
Casey Mordini-Bluhm <caseyb108@gmail.com>
2018-06-04 13:47:53 -0700

    Fixed multiple key press problem. Made QTimer, xvelocity, members of player.

--- Phase_Changer_Game/phase_changer.h
+++ Phase_Changer_Game/phase_changer.h
@@ -3,18 +3,28 @@
 
 #include <QGraphicsEllipseItem>
 #include <QObject>
+#include <QTimer>
 
 class phase_changer : public QObject, public QGraphicsEllipseItem {
     Q_OBJECT
 private:
     int phase;
+    int xvelocity;
+    int yvelocity;
+
+    QTimer * timer;
+
 public:
     phase_changer();
 
+
+
     void keyPressEvent(QKeyEvent * event);
+    void keyReleaseEvent(QKeyEvent * event);
 
 public slots:
     void move();
+    void traverse();
     void fall();
     void rise();
 
